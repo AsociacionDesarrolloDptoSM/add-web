258a1f68bbc0804546e9c0236c564c611151ac6d
AsociacionDesarrolloDptoSM <asociacionparaeldesarrollosm@gmail.com>
2026-07-22 17:40:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,42 @@ function cerrarImagen(){
 }
 
 </script>
+<script>
+
+const elementos = document.querySelectorAll(
+'.card, .mv-card, .galeria img, .banners img, .titulo'
+);
+
+elementos.forEach(elemento=>{
+    elemento.classList.add('animar');
+});
+
+
+const observer = new IntersectionObserver((entradas)=>{
+
+    entradas.forEach(entrada=>{
+
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('visible');
+        }
+
+    });
+
+},{
+    threshold:0.15
+});
+
+
+elementos.forEach(elemento=>{
+    observer.observe(elemento);
+});
+
+</script>
+    
+<script>
+
+function toggleMenu(){
+    document.getElementById("menu").classList.toggle("activo");
+}
+
+</script>    
