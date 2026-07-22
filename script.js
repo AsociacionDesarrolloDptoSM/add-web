1615f4c0b4ccfc3193311361b4cd4581efe39670
AsociacionDesarrolloDptoSM <asociacionparaeldesarrollosm@gmail.com>
2026-07-22 17:41:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -127,3 +127,42 @@ function toggleMenu(){
 }
 
 </script>    
+<script>
+
+const elementos = document.querySelectorAll(
+'.card, .mv-card, .galeria img, .banners img, .titulo'
+);
+
+elementos.forEach(elemento=>{
+    elemento.classList.add('animar');
+});
+
+
+const observer = new IntersectionObserver((entradas)=>{
+
+    entradas.forEach(entrada=>{
+
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('visible');
+        }
+
+    });
+
+},{
+    threshold:0.15
+});
+
+
+elementos.forEach(elemento=>{
+    observer.observe(elemento);
+});
+
+</script>
+    
+<script>
+
+function toggleMenu(){
+    document.getElementById("menu").classList.toggle("activo");
+}
+
+</script>    
